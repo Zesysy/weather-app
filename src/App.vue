08f900ba2c31e7e4b6bc23ccf26ff3d90ffcbb45
Sylène <sylene.manusset@gmail.com>
2020-06-29 14:17:50 +0200

    Clean code to start the development of the application and addition of a title + favicon in public/

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
+<!-- Entry point of the VueJs application -->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  Hello World
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ 
 }
 </style>
