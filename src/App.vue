1657ebd2209a10bc6a034620a318e7f7fb5957e8
Sylène <sylene.manusset@gmail.com>
2020-07-11 16:54:30 +0200

    [DONE] Html page and its style finished: addition of the sought zone, the date of the day, the temperature and the weather

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,16 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search..." />
       </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Venise, IT</div>
+          <div class="date">Samedi 11 juillet 2020</div>
+        </div>
+        <div class="weather-box">
+          <div class="temp">24°c</div>
+          <div class="weather">Soleil</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -76,6 +86,45 @@ main {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #fff;
+  font-size: 30px;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 18px;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 100px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 46px;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
 
